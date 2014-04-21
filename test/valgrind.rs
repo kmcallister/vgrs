e524216b3d66ea81f625e9323206223f35c1295e
Keegan McAllister <kmcallister@mozilla.com>
2014-04-21 16:55:03 -0700

    Add Makefile and start of tests

---
+++ test/valgrind.rs
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![crate_id="vgrs-valgrind-test"]
+#![crate_type="bin"]
+
+extern crate vgrs;
+
+use vgrs::valgrind;
+
+fn main() {
+    unsafe {
+        assert_eq!(valgrind::running_on_valgrind(), 1);
+    }
+}
