43eefc18d8dca1c5f8719eb1aae691dfbc8f255b
Keegan McAllister <kmcallister@mozilla.com>
2014-10-01 17:13:28 -0700

    Don't warn about request enums we haven't wrapped yet

--- src/enums.rs
+++ src/enums.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![allow(non_camel_case_types)]
+#![allow(non_camel_case_types, dead_code)]
 
 macro_rules! VG_USERREQ_TOOL_BASE ( ($a:expr, $b:expr) => (
     ((($a as int) & 0xff) << 24)
